a5a89f2aa84c2ab18a7ce1d87e9deb05d1ddcaf4
rjlam <rjlampe12@gmail.com>
2019-11-16 21:37:58 -0600

    java portion of lab completed (need to handin and run in webgrader)

--- src/unl/cse/Birthday.java
+++ src/unl/cse/Birthday.java
@@ -4,32 +4,36 @@ import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.joda.time.Period;
 
-public class Birthday {public static void main(String args[]) {
+public class Birthday {
+	public static void main(String args[]) {
+
+		String name = "Sam I Am";
+
+		int month = 4;
+		int date = 24;
+		int year = 2000;
+
+		DateTime bday = new DateTime(year, month, date, 0, 0);
+		DateTime today = new DateTime();
+
+		Period age = new Period(bday, today);
+
+		int years = age.getYears();
+		int months = age.getMonths();
+		int days = age.getWeeks() * 7 + age.getDays();
+
+		DateTime nextBDay = new DateTime(year + years + 1, month, date, 0, 0);
+		Interval daysToNextBDay = new Interval(today, nextBDay);
+		double daysRemaining = daysToNextBDay.toDurationMillis() / (1000 * 60 * 60 * 24) + 1;
+
+		// TODO: write code to output results here
+		System.out.printf("Greetings, %s, Today you are %d years, %d months, and %d days old.\n", name, years, months, days);
 		
-String name = "Java";
+		if(daysRemaining == 0.0) {
+			System.out.println("Happy Birthday!");
+		}else {
+			System.out.println("Your friends have " + daysRemaining + " days to shop for you!");
+		}
 
-int 
-month = 1;
-int date  = 23;
-int year  = 1996;
-
-DateTime              bday = 
-		   new DateTime(year, month, date, 0, 0);
-DateTime today = new DateTime();
-
-Period age = new Period(bday, today);
-
-int years = age.getYears();
-int months = age.getMonths();
-int days = age.getWeeks() * 7 + age.getDays();
-
-DateTime next_bday = new DateTime(year + years + 1, month, date, 0, 0);
-Interval days_to_next_bday_i = 
-  new Interval(today, next_bday);
-double days_remaining = 
-  days_to_next_bday_i.toDurationMillis() / (1000 * 60 * 60 * 24) + 1;
-
-                  //TODO: write code to output results here
-		
-}
+	}
 }
